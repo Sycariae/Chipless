47a5f2c3d7aac88399edd3a078faf935ecbc263e
Sycariae <superhydro8@gmail.com>
2024-12-01 17:00:39 +0000

    Created local PlayerButton composable, added DealerIcon functionality, implemented with demo names

--- app/src/main/java/com/sycarias/chipless/ui/screens/CreateTable.kt
+++ app/src/main/java/com/sycarias/chipless/ui/screens/CreateTable.kt
@@ -15,9 +15,13 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.sycarias.chipless.R
+import com.sycarias.chipless.ui.composables.DealerIcon
 import com.sycarias.chipless.ui.composables.IntInputField
 import com.sycarias.chipless.ui.composables.PlayerButton
 import com.sycarias.chipless.ui.composables.StaticShadow
@@ -27,14 +31,50 @@ import com.sycarias.chipless.ui.theme.ChiplessColors
 import com.sycarias.chipless.ui.theme.ChiplessShadowStyle
 import com.sycarias.chipless.ui.theme.ChiplessTypography
 
+class DealerViewModel: ViewModel() {
+    // Holds the ID or index of the active dealer
+    private val _activeDealerId = mutableStateOf<Int?>(null)
+    val activeDealerId: State<Int?> = _activeDealerId
+
+    // Function to update the active dealer
+    fun setActiveDealer(id: Int) {
+        _activeDealerId.value = id
+    }
+}
+
+enum class PlayerButtonLocation {
+    TOP_ROW,
+    MID_SECTION,
+    BOTTOM_ROW
+}
+
+enum class PlayerButtonSide {
+    LEFT,
+    RIGHT
+}
+
 @Suppress("UNUSED_PARAMETER")
 @Composable
 fun CreateTable(navController: NavController) {
-    // Default Values for Text Fields
+    // Define Sizing and Spacing for Dealer Icons
+    val dealerIconSize = 30.dp // Sizing of Dealer Icons
+    val dealerIconMidSpacing = 5.dp // Spacing Between Player Button and Dealer Icon in Mid-Section
+    val dealerIconTBRHSpacing = -6.dp // Horizontal Spacing Between Player Button and Dealer Icon in Top and Bottom Rows
+    val dealerIconTBRVSpacing = -10.dp // Vertical Spacing Between Player Button and Dealer Icon in Top and Bottom Rows
+
+    // Define Sizing and Spacing for Player Buttons
+    val playerButtonSize = 50.dp // Sizing of Player Buttons
+    val playerButtonMidVSpacing = 32.dp // Player Button Vertical Spacing
+    val playerButtonTBRVSpacing = playerButtonMidVSpacing /*- dealerIconTBRVSpacing*/ // Reduce spacing to accommodate for additional clearance
+    val playerButtonMidHSpacing = 175.dp /*- (dealerIconMidClearance * 2)*/ // Player Button Middle Horizontal Spacing
+    val playerButtonTBRHSpacing = 88.dp /*- (dealerIconTBRHClearance * 2)*/ // Player Button Top and Bottom Row Horizontal Spacing
+
+    // Default Values for Input Fields
     var startingChips by remember { mutableIntStateOf(1000) }
     var bigBlind by remember { mutableIntStateOf(10) }
     var smallBlind by remember { mutableIntStateOf(5) }
 
+    // Input Field Validation
     val startingChipsValid by remember {
         derivedStateOf {
             startingChips in bigBlind * 10 .. 1000000
@@ -51,10 +91,95 @@ fun CreateTable(navController: NavController) {
         }
     }
 
-    // Add Player Button Spacing
-    val aPBVSpacing = 32.dp // Vertical Spacing
-    val aPBRowHSpacing = 88.dp // Top and Bottom Row Horizontal Spacing
-    val aPBMidHSpacing = 175.dp // Middle Horizontal Spacing
+    val names = remember {
+        mutableStateListOf(*Array(10) { "" })
+    }
+
+    // Dealer Icon States
+    val viewModel: DealerViewModel = viewModel()
+    val activeDealerId by viewModel.activeDealerId
+
+    // Local Dealer Icon Composable using Identifiers
+    @Composable
+    fun CTSDealerIcon(id:Int) { // Create Table Screen Dealer Icon
+        DealerIcon(
+            active = (id == activeDealerId),
+            size = dealerIconSize,
+            onClick = { viewModel.setActiveDealer(id) }
+        )
+    }
+
+    // Local Player Button Composable using Identifiers
+    @Composable
+    fun CTSPlayerButton(
+        id: Int,
+        location: PlayerButtonLocation,
+        side: PlayerButtonSide,
+        onClick: () -> Unit = {}
+    ) {
+        fun checkForInversion(x: Dp): Dp {
+            return if (side == PlayerButtonSide.RIGHT) { -x }
+            else x
+        }
+
+        val offsetX: Dp = when {
+            location == PlayerButtonLocation.MID_SECTION ->
+                checkForInversion(
+                    dealerIconMidSpacing + dealerIconSize
+                )
+            else ->
+                checkForInversion(
+                    dealerIconTBRHSpacing + dealerIconSize
+                )
+        }
+
+        val offsetY: Dp = when (location) {
+            PlayerButtonLocation.TOP_ROW ->
+                dealerIconTBRVSpacing + dealerIconSize
+            PlayerButtonLocation.MID_SECTION -> 0.dp
+            PlayerButtonLocation.BOTTOM_ROW ->
+                -( dealerIconTBRVSpacing + dealerIconSize )
+        }
+
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = when (side) {
+                PlayerButtonSide.LEFT -> Arrangement.End
+                PlayerButtonSide.RIGHT -> Arrangement.Start
+            }
+        ) {
+            Box (
+                contentAlignment = when (location) { // Determine Alignment based on positional args
+                    PlayerButtonLocation.TOP_ROW ->
+                        when (side) {
+                            PlayerButtonSide.LEFT -> Alignment.BottomEnd
+                            PlayerButtonSide.RIGHT -> Alignment.BottomStart
+                        }
+                    PlayerButtonLocation.MID_SECTION ->
+                        when (side) {
+                            PlayerButtonSide.LEFT -> Alignment.CenterEnd
+                            PlayerButtonSide.RIGHT -> Alignment.CenterStart
+                        }
+                    PlayerButtonLocation.BOTTOM_ROW ->
+                        when (side) {
+                            PlayerButtonSide.LEFT -> Alignment.TopEnd
+                            PlayerButtonSide.RIGHT -> Alignment.TopStart
+                        }
+                }
+            ) {
+                PlayerButton(
+                    name = names[id],
+                    size = playerButtonSize,
+                    onClick = onClick
+                )
+                if (names[id].isNotEmpty()) {
+                    Box(
+                        modifier = Modifier.offset(offsetX, offsetY)
+                    ) { CTSDealerIcon(id = id) }
+                }
+            }
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -133,40 +258,40 @@ fun CreateTable(navController: NavController) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                val name1 = remember {
-                    mutableStateOf("Luke")
-                }
-                val name2 = remember {
-                    mutableStateOf("")
-                }
                 Row( // PLAYERS TOP ROW
                     modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center
                 ) {
-                    Row (
-                        modifier = Modifier.weight(1f),
-                        horizontalArrangement = Arrangement.End
-                    ) {
-                        PlayerButton(name = name1.value,
+                    Row (modifier = Modifier.weight(1f)) {
+                        CTSPlayerButton(
+                            id = 0,
+                            side = PlayerButtonSide.LEFT,
+                            location = PlayerButtonLocation.TOP_ROW,
                             onClick = {
-                                name1.value = ""
+                                names[0] = when {
+                                    names[0].isEmpty() -> "Luke"
+                                    else -> ""
+                                }
                             }
                         )
                     }
-                    Spacer(modifier = Modifier.width(aPBRowHSpacing))
-                    Row (
-                        modifier = Modifier.weight(1f),
-                        horizontalArrangement = Arrangement.Start
-                    ) {
-                        PlayerButton(name = name2.value,
+                    Spacer(modifier = Modifier.width(playerButtonTBRHSpacing))
+                    Row (modifier = Modifier.weight(1f)) {
+                        CTSPlayerButton(
+                            id = 1,
+                            side = PlayerButtonSide.RIGHT,
+                            location = PlayerButtonLocation.TOP_ROW,
                             onClick = {
-                                name2.value = "Adam"
+                                names[1] = when {
+                                    names[1].isEmpty() -> "Alice"
+                                    else -> ""
+                                }
                             }
                         )
                     }
                 }
-                Spacer(modifier = Modifier.height(aPBVSpacing))
+                Spacer(modifier = Modifier.height(playerButtonTBRVSpacing))
                 Row( // PLAYER MIDDLE SECTION
                     modifier = Modifier
                         .fillMaxWidth(),
@@ -177,47 +302,117 @@ fun CreateTable(navController: NavController) {
                         modifier = Modifier.weight(1f),
                         horizontalAlignment = Alignment.End
                     ) { // LEFT
-                        PlayerButton(name = name1.value,
+                        CTSPlayerButton(
+                            id = 2,
+                            side = PlayerButtonSide.LEFT,
+                            location = PlayerButtonLocation.MID_SECTION,
                             onClick = {
-                                name1.value = ""
+                                names[2] = when {
+                                    names[2].isEmpty() -> "Anjali"
+                                    else -> ""
+                                }
                             }
                         )
-                        Spacer(modifier = Modifier.height(aPBVSpacing))
-                        PlayerButton()
-                        Spacer(modifier = Modifier.height(aPBVSpacing))
-                        PlayerButton()
+                        Spacer(modifier = Modifier.height(playerButtonMidVSpacing))
+                        CTSPlayerButton(
+                            id = 3,
+                            side = PlayerButtonSide.LEFT,
+                            location = PlayerButtonLocation.MID_SECTION,
+                            onClick = {
+                                names[3] = when {
+                                    names[3].isEmpty() -> "Nonrad"
+                                    else -> ""
+                                }
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(playerButtonMidVSpacing))
+                        CTSPlayerButton(
+                            id = 4,
+                            side = PlayerButtonSide.LEFT,
+                            location = PlayerButtonLocation.MID_SECTION,
+                            onClick = {
+                                names[4] = when {
+                                    names[4].isEmpty() -> "Ian"
+                                    else -> ""
+                                }
+                            }
+                        )
                     }
-                    Spacer(modifier = Modifier.width(aPBMidHSpacing))
+                    Spacer(modifier = Modifier.width(playerButtonMidHSpacing))
                     Column(
                         modifier = Modifier.weight(1f),
                         horizontalAlignment = Alignment.Start
                     ) { // RIGHT
-                        PlayerButton()
-                        Spacer(modifier = Modifier.height(aPBVSpacing))
-                        PlayerButton()
-                        Spacer(modifier = Modifier.height(aPBVSpacing))
-                        PlayerButton()
+                        CTSPlayerButton(
+                            id = 5,
+                            side = PlayerButtonSide.RIGHT,
+                            location = PlayerButtonLocation.MID_SECTION,
+                            onClick = {
+                                names[5] = when {
+                                    names[5].isEmpty() -> "Seb"
+                                    else -> ""
+                                }
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(playerButtonMidVSpacing))
+                        CTSPlayerButton(
+                            id = 6,
+                            side = PlayerButtonSide.RIGHT,
+                            location = PlayerButtonLocation.MID_SECTION,
+                            onClick = {
+                                names[6] = when {
+                                    names[6].isEmpty() -> "E-van"
+                                    else -> ""
+                                }
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(playerButtonMidVSpacing))
+                        CTSPlayerButton(
+                            id = 7,
+                            side = PlayerButtonSide.RIGHT,
+                            location = PlayerButtonLocation.MID_SECTION,
+                            onClick = {
+                                names[7] = when {
+                                    names[7].isEmpty() -> "Adam"
+                                    else -> ""
+                                }
+                            }
+                        )
                     }
                 }
-                Spacer(modifier = Modifier.height(aPBVSpacing))
+                Spacer(modifier = Modifier.height(playerButtonTBRVSpacing))
                 Row( // PLAYERS BOTTOM ROW
                     modifier = Modifier
                         .fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center
                 ) {
-                    Row (
-                        modifier = Modifier.weight(1f),
-                        horizontalArrangement = Arrangement.End
-                    ) {
-                        PlayerButton { /*TODO APB*/ }
+                    Row (modifier = Modifier.weight(1f)) {
+                        CTSPlayerButton(
+                            id = 8,
+                            side = PlayerButtonSide.LEFT,
+                            location = PlayerButtonLocation.BOTTOM_ROW,
+                            onClick = {
+                                names[8] = when {
+                                    names[8].isEmpty() -> "Jodie"
+                                    else -> ""
+                                }
+                            }
+                        )
                     }
-                    Spacer(modifier = Modifier.width(aPBRowHSpacing))
-                    Row (
-                        modifier = Modifier.weight(1f),
-                        horizontalArrangement = Arrangement.Start
-                    ) {
-                        PlayerButton { /*TODO APB*/ }
+                    Spacer(modifier = Modifier.width(playerButtonTBRHSpacing))
+                    Row (modifier = Modifier.weight(1f)) {
+                        CTSPlayerButton(
+                            id = 9,
+                            side = PlayerButtonSide.RIGHT,
+                            location = PlayerButtonLocation.BOTTOM_ROW,
+                            onClick = {
+                                names[9] = when {
+                                    names[9].isEmpty() -> "Kody"
+                                    else -> ""
+                                }
+                            }
+                        )
                     }
                 }
             }
@@ -232,9 +427,8 @@ fun CreateTable(navController: NavController) {
         verticalArrangement = Arrangement.Bottom
     ) {
         Button(
-            onClick = { /* TODO: Menu Button Click */ },
+            onClick = { navController.navigate("GameTable") },
             modifier = Modifier
-                .width(135.dp)
                 .height(55.dp)
                 .buttonShadow(
                     color = ChiplessColors.primary,
@@ -255,10 +449,11 @@ fun CreateTable(navController: NavController) {
                 Icon(
                     painter = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.icon_play)),
                     contentDescription = "Settings",
+                    tint = ChiplessColors.textPrimary,
                     modifier = Modifier.size(18.dp)
                 )
             }
-            Spacer(modifier = Modifier.width(10.dp))
+            Spacer(modifier = Modifier.width(8.dp))
             Text(
                 "Play",
                 color = ChiplessColors.textPrimary,
