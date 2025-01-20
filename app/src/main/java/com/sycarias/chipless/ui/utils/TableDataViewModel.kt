59a300e927a0544952744b6c0a487ea1c2d458d9
Sycariae <superhydro8@gmail.com>
2025-01-20 21:38:17 +0000

    Implemented Players Class: replaced _playerNames, _playerBalances, _playerCurrentBets, _playerStatuses, _activeDealerID and participatingPlayers; added more and improved helper functions; added and adjusted comments

--- app/src/main/java/com/sycarias/chipless/ui/utils/TableDataViewModel.kt
+++ app/src/main/java/com/sycarias/chipless/ui/utils/TableDataViewModel.kt
@@ -1,139 +1,100 @@
 package com.sycarias.chipless.ui.utils
 
-import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class TableDataViewModel: ViewModel() {
     // = TABLE CONFIG
+    // The starting balance given out at the start of a match
     private val _startingChips = mutableIntStateOf(1000)
     val startingChips: State<Int> = _startingChips
-
+    // The minimum bet and the forced bet made by 2nd the player after the dealer at the start of a match
     private val _bigBlind = mutableIntStateOf(10)
     val bigBlind: State<Int> = _bigBlind
-
+    // The forced bet made by the player after the dealer at the start of a match
     private val _smallBlind = mutableIntStateOf(5)
     val smallBlind: State<Int> = _smallBlind
 
+    // Table Config Setter Functions
     fun setStartingChips(value: Int) {
         _startingChips.intValue = value
     }
-
     fun setBigBlind(value: Int) {
         _bigBlind.intValue = value
     }
-
     fun setSmallBlind(value: Int) {
         _smallBlind.intValue = value
     }
 
 
-    // = DEALER SELECTION
-    private val _activeDealerId: MutableState<Int?> = mutableStateOf(null)
-    val activeDealerId: State<Int?> = _activeDealerId
-
-    // Update the active dealer
-    fun setActiveDealer(playerID: Int) {
-        _activeDealerId.value = playerID
-    }
+    // = PLAYERS
+    private val _players by mutableStateOf( Players(10) )
+    val players: Players = _players
 
 
     // = BETS AND BALANCES
-    private val _playerBalances = mutableStateListOf(*Array(10) { _startingChips.intValue })
-    val playerBalances: SnapshotStateList<Int> = _playerBalances
-
-    private val _playerCurrentBets = mutableStateListOf(*Array(10) { 0 })
-    val playerCurrentBets: SnapshotStateList<Int> = _playerCurrentBets
-
-    private val _currentTableBet = derivedStateOf { _playerCurrentBets.maxOrNull() ?: 0}
+    // The bet players must match for the current betting round
+    private val _currentTableBet = _players.highestBet
     val currentTableBet: State<Int> = _currentTableBet
-
+    // The running total of all bets made this match that'll be shared amongst the winners at the end
     private val _tablePot = mutableIntStateOf(0)
     val tablePot: State<Int> = _tablePot
 
+    // Bet Placement Helper Functions
     fun callBet(playerID: Int) {
-        placeBet(playerID, betAmount = _currentTableBet.value)
+        placeBet(playerID = playerID, betAmount = _currentTableBet.value, isRaise = false)
     }
-
-    fun placeBet(playerID: Int, betAmount: Int, isRaise: Boolean = false) {
-        // Add bet amount to table pot and set table bet
-        _tablePot.intValue += betAmount
-
-        // Update bet amount and balance for betting player
-        _playerCurrentBets[playerID] += betAmount
-        _playerBalances[playerID] -= betAmount
-
-        updateStatusesOnAction(playerID, isRaise = isRaise)
+    fun raiseBet(playerID: Int, raiseAmount: Int) {
+        placeBet(playerID = playerID, betAmount = _currentTableBet.value + raiseAmount, isRaise = true)
     }
-
-
-    // = PLAYER NAMES
-    private val _playerNames = mutableStateListOf(*Array(10) { "" })
-    val playerNames: SnapshotStateList<String> = _playerNames
-    val playerCount = derivedStateOf { _playerNames.count { it.isNotBlank() } }
-
-    // Update a specific player's name
-    fun updatePlayerName(playerID: Int, name: String) {
-        _playerNames[playerID] = name
-    }
-
-
-    // = PLAYER STATUSES
-    private val _playerStatuses = mutableStateListOf(*Array(10) { PlayerStatus.IDLE })
-    val playerStatuses: SnapshotStateList<PlayerStatus> = _playerStatuses
-
-    // Update a specific player's status
-    fun updatePlayerStatus(playerID: Int, status: PlayerStatus) {
-        _playerStatuses[playerID] = status
-    }
-
-    // Update all statuses when someone raises
-    private fun updateStatusesOnAction(actionPlayerID: Int, isRaise: Boolean = false) {
-        _playerStatuses[actionPlayerID] = if (isRaise) PlayerStatus.RAISED else PlayerStatus.BET_MATCHED
-
-        // Adjust statuses for other participating players
-        _playerStatuses.forEachIndexed { playerID, status ->
-            if (status == PlayerStatus.BET_MATCHED) {
-                _playerStatuses[playerID] = PlayerStatus.PARTIAL_MATCH
-            }
+    fun allInBet(playerID: Int) {
+        val isRaise = when { // Determine whether the ALL_IN bet is a raise or not
+            _players.getPlayerBalance(playerID) > _currentTableBet.value -> true
+            else -> false
         }
+
+        placeBet(playerID = playerID, betAmount = _players.getPlayerBalance(playerID), isRaise = isRaise)
+
+        _players.setPlayerStatus(playerID = playerID, status = PlayerStatus.ALL_IN)
+    }
+    fun placeForcedBets() {
+        placeBet(playerID = _players.smallBlindPlayer, betAmount = _smallBlind.intValue, isRaise = false) // Place forced bet for small blind player
+        placeBet(playerID = _players.bigBlindPlayer, betAmount = _bigBlind.intValue, isRaise = false) // Place forced bet for big blind player
+
+        _players.setPlayerStatus(playerID = _players.smallBlindPlayer, status = PlayerStatus.PARTIAL_MATCH) // Ensure small blind player is given PARTIAL_MATCH status without making the big blind player RAISED status
+    }
+    private fun placeBet(playerID: Int, betAmount: Int, isRaise: Boolean) {
+        _tablePot.intValue += betAmount // Add bet amount to table pot and set table bet
+
+        _players.placePlayerBet(playerID = playerID, amount = betAmount) // Update bet amount and balance for betting player
+
+        _players.updateStatusesOnBet(betterID = playerID, isRaise = isRaise) // Update player statuses: on raise, all BET_MATCHED are updated to PARTIAL_MATCH
     }
 
+    // Distribute the table pot amongst the winners
+    fun distributeWinnings(playerIDs: List<Int>) {
+        val winningsSplit = _tablePot.intValue / playerIDs.count() // Calculate an even split amongst the winners
 
-    // = PARTICIPATING PLAYERS
-    val participatingPlayers by derivedStateOf {
-        _playerNames.indices.filter { playerID ->
-            _playerNames[playerID].isNotBlank() && _playerStatuses[playerID] !in listOf(PlayerStatus.FOLDED, PlayerStatus.SAT_OUT)
+        playerIDs.forEach { playerID ->
+            _players.payPlayer(playerID = playerID, amount = winningsSplit) // Award each player their split
         }
+        _tablePot.intValue = 0 // Reset the pot value back to 0
     }
 
 
-    // = ACTING PLAYER
-    private val _actingPlayer = mutableIntStateOf(activeDealerId.value ?: participatingPlayers.first())
-    val actingPlayer: State<Int> = _actingPlayer
-
-    // Update the acting player id
-    fun updateActivePlayer(playerID: Int) {
-        _actingPlayer.intValue = playerID
-    }
-
-    // Increment to the next acting player (filtering out empty and inactive players)
-    fun incrementActingPlayer() {
-        val nextPlayerID = participatingPlayers.indexOfFirst { it > _actingPlayer.intValue }
-
-        _actingPlayer.intValue = if (nextPlayerID != -1) {
-            participatingPlayers[nextPlayerID]
-        } else {
-            participatingPlayers.firstOrNull() ?: 0
-        }
-    }
-
-
-    // = GAME STAGE
+    // = BETTING ROUND
+    // The current round of betting ( e.g. PREFLOP, FLOP, TURN... )
     private val _bettingRound = mutableStateOf(BettingRound.PREFLOP)
     val bettingRound: State<BettingRound> = _bettingRound
 
-    // Update game stage to next stage
+    // Update betting round to PREFLOP
+    private fun resetBettingRound() {
+        _bettingRound.value = BettingRound.PREFLOP
+    }
+    // Update betting round to next round
     private fun incrementGameStage() {
         _bettingRound.value = when(_bettingRound.value) {
             BettingRound.PREFLOP -> BettingRound.FLOP
@@ -145,29 +106,20 @@ class TableDataViewModel: ViewModel() {
     }
 
 
-    // = RESETS
+    // = INITIALISATION
     fun initiateNewRound() {
-        // Reset player statuses excluding FOLDED, ALL_IN and SAT_OUT
-        _playerStatuses.replaceAll { status ->
-            when(status) {
-                PlayerStatus.FOLDED -> PlayerStatus.FOLDED
-                PlayerStatus.ALL_IN -> PlayerStatus.ALL_IN
-                PlayerStatus.SAT_OUT -> PlayerStatus.SAT_OUT
-                else -> PlayerStatus.IDLE
-            }
-        }
-        _playerCurrentBets.fill(0) // Reset all current player bets
-
+        _players.resetAllForNewRound() // Reset player statuses excluding FOLDED, ALL_IN and SAT_OUT and reset all current player bets
         incrementGameStage() // Set game stage to next stage from the current one
     }
-
     fun initiateNewMatch() {
-        // Reset player statuses excluding SAT_OUT
-        _playerStatuses.replaceAll { status ->
-            if (status != PlayerStatus.SAT_OUT) PlayerStatus.IDLE else PlayerStatus.SAT_OUT
-        }
-        _playerCurrentBets.fill(0) // Reset all current player bets
-
-        _bettingRound.value = BettingRound.PREFLOP // Set game stage to first stage: PREFLOP
+        _players.resetAllForNewMatch() // Reset player statuses excluding SAT_OUT and all current player bets
+        resetBettingRound() // Set game stage to first stage: PREFLOP
+    }
+    // Ran when first starting the table, performs all resets and ensures players are given correct starting balances
+    fun initialiseNewTable() {
+        _players.resetAllForNewTable() // Reset all player statuses and reset all current player bets
+        _players.setStartingBalances(_startingChips.intValue) // Set all player balances to startingChips
+        _players.setInitialFocusPlayer() // Set focus player to the 3rd player after the dealer
+        resetBettingRound() // Set betting round to PREFLOP
     }
 }
