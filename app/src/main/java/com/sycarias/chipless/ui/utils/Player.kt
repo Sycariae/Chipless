baa57ca8b32a9de9115f09a58260305a4b27319e
Sycariae <superhydro8@gmail.com>
2025-01-20 21:37:45 +0000

    Added Player Class to manage names, statuses, balances and bets of players

---
+++ app/src/main/java/com/sycarias/chipless/ui/utils/Player.kt
@@ -0,0 +1,47 @@
+package com.sycarias.chipless.ui.utils
+
+data class Player(
+    var name: String = "",
+    var status: PlayerStatus = PlayerStatus.IDLE,
+    var balance: Int = 0,
+    var currentBet: Int = 0
+) {
+    fun setName(newName: String) {
+        name = newName
+    }
+
+    fun setStatus(newStatus: PlayerStatus) {
+        status = newStatus
+    }
+
+    fun setBalance(amount: Int) {
+        balance = amount
+    }
+
+    fun pay(amount: Int) {
+        balance += amount
+    }
+
+    fun bet(amount: Int) {
+        currentBet += amount
+        balance -= amount
+    }
+
+    // = RESETS
+    fun resetForNewRound() {
+        currentBet = 0
+        if (status !in listOf(PlayerStatus.SAT_OUT, PlayerStatus.FOLDED, PlayerStatus.ALL_IN)) {
+            status = PlayerStatus.IDLE
+        }
+    }
+    fun resetForNewMatch() {
+        currentBet = 0
+        if (status != PlayerStatus.SAT_OUT) {
+            status = PlayerStatus.IDLE
+        }
+    }
+    fun resetForNewTable() {
+        currentBet = 0
+        status = PlayerStatus.IDLE
+    }
+}
