182591a0fcb3454ed729ff77d87d8ff396d3e7f7
Sycariae <superhydro8@gmail.com>
2025-01-24 11:50:29 +0000

    Fixed logic error by changing Player Data Class to Class and defining variables with `by mutableStateOf()`

--- app/src/main/java/com/sycarias/chipless/viewModel/Player.kt
+++ app/src/main/java/com/sycarias/chipless/viewModel/Player.kt
@@ -1,11 +1,16 @@
 package com.sycarias.chipless.viewModel
 
-data class Player(
-    var name: String = "",
-    var status: PlayerStatus = PlayerStatus.IDLE,
-    var balance: Int = 0,
-    var currentBet: Int = 0
-) {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class Player {
+    var name: String by mutableStateOf("")
+    var status: PlayerStatus by mutableStateOf(PlayerStatus.IDLE)
+    var balance: Int by mutableIntStateOf(0)
+    var currentBet: Int by mutableIntStateOf(0)
+
     fun pay(amount: Int) {
         balance += amount
     }
