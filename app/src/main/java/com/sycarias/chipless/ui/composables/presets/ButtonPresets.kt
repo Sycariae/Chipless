7e0e99be0bc12d324cbdf5ed7dbdc77910a02485
Sycariae <superhydro8@gmail.com>
2025-02-03 14:33:20 +0000

    Added Button and Typography Presets, made several amendments to this commit to increase modularity

---
+++ app/src/main/java/com/sycarias/chipless/ui/composables/presets/ButtonPresets.kt
@@ -0,0 +1,84 @@
+package com.sycarias.chipless.ui.composables.presets
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import com.sycarias.chipless.ui.extensions.buttonShadow
+import com.sycarias.chipless.ui.theme.ChiplessButtonColors
+import com.sycarias.chipless.ui.theme.ChiplessColors
+
+@Composable
+fun LargeFocusButton(
+    onClick: () -> Unit,
+    contents: @Composable () -> Unit = {}
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(320.dp)
+            .height(75.dp)
+            .buttonShadow(
+                color = ChiplessColors.primary,
+                offsetX = 0.dp,
+                offsetY = 0.dp,
+                blurRadius = 30.dp,
+                cornerRadius = 100.dp
+            ),
+        shape = RoundedCornerShape(100.dp),
+        colors = ChiplessButtonColors(ChiplessColors.primary)
+    ) {
+        contents()
+    }
+}
+
+@Composable
+fun LargeButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+        .width(290.dp)
+        .height(65.dp),
+    shape: Shape = RoundedCornerShape(100.dp),
+    buttonColors: ButtonColors = ChiplessButtonColors(),
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(5.dp),
+    contents: @Composable () -> Unit = {}
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = shape,
+        colors = buttonColors,
+        elevation = elevation
+    ) {
+        contents()
+    }
+}
+
+@Composable
+fun ActionButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+        .width(120.dp)
+        .height(55.dp),
+    shape: Shape = RoundedCornerShape(100.dp),
+    colors: ButtonColors = ChiplessButtonColors(),
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(5.dp),
+    contents: @Composable () -> Unit = {}
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = shape,
+        colors = colors,
+        elevation = elevation
+    ) {
+        contents()
+    }
+}
---
+++ app/src/main/java/com/sycarias/chipless/ui/composables/presets/TypographyPresets.kt
@@ -0,0 +1,178 @@
+package com.sycarias.chipless.ui.composables.presets
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import com.sycarias.chipless.ui.theme.ChiplessColors
+import com.sycarias.chipless.ui.theme.ChiplessShadowStyle
+import com.sycarias.chipless.ui.theme.ChiplessTypography
+
+@Composable
+fun Title(
+    text: String,
+    color: Color = ChiplessColors.textPrimary,
+    textAlign: TextAlign? = TextAlign.Center
+) {
+    Text(
+        text = text,
+        style = ChiplessShadowStyle(
+            style = ChiplessTypography.title,
+            offsetX = -12f,
+            offsetY = 12f,
+            blurRadius = 25f
+        ),
+        color = color,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun Heading(
+    text: String,
+    color: Color = ChiplessColors.textPrimary,
+    textAlign: TextAlign? = TextAlign.Center
+) {
+     Text(
+         text = text,
+         style = ChiplessShadowStyle(
+             style = ChiplessTypography.h1,
+             offsetX = -12f,
+             offsetY = 12f,
+             blurRadius = 25f
+         ),
+         color = color,
+         textAlign = textAlign
+     )
+}
+
+@Composable
+fun Subtitle(
+    text: String,
+    color: Color = ChiplessColors.textSecondary,
+    textAlign: TextAlign? = TextAlign.Center
+) {
+    Text(
+        text = text,
+        style = ChiplessShadowStyle(
+            style = ChiplessTypography.h2,
+            offsetX = -12f,
+            offsetY = 12f,
+            blurRadius = 25f
+        ),
+        color = color,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun Subheading(
+    text: String,
+    color: Color = ChiplessColors.textTertiary,
+    textAlign: TextAlign? = TextAlign.Center
+) {
+    Text(
+        text = text,
+        style = ChiplessShadowStyle(
+            style = ChiplessTypography.sh2,
+            offsetX = -8f,
+            offsetY = 8f,
+            blurRadius = 20f
+        ),
+        color = color,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun Body(
+    text: String,
+    color: Color = ChiplessColors.textSecondary,
+    textAlign: TextAlign? = null,
+    softWrap: Boolean = true
+) {
+    Text(
+        text = text,
+        style = ChiplessShadowStyle(
+            style = ChiplessTypography.body,
+            offsetX = -8f,
+            offsetY = 8f,
+            blurRadius = 15f
+        ),
+        color = color,
+        textAlign = textAlign,
+        softWrap = softWrap
+    )
+}
+
+@Composable
+fun Label(
+    text: String,
+    color: Color = ChiplessColors.textSecondary,
+    textAlign: TextAlign? = null,
+    softWrap: Boolean = true
+) {
+    Text(
+        text = text,
+        style = ChiplessShadowStyle(
+            style = ChiplessTypography.l2,
+            offsetX = -6f,
+            offsetY = 6f,
+            blurRadius = 15f
+        ),
+        color = color,
+        textAlign = textAlign,
+        softWrap = softWrap
+    )
+}
+
+@Composable
+fun LargeFocusButtonText(
+    text: String,
+    color: Color = ChiplessColors.textPrimary
+) {
+    Text(
+        text = text,
+        style = ChiplessShadowStyle(
+            style = ChiplessTypography.h2,
+            offsetX = -12f,
+            offsetY = 12f,
+            blurRadius = 25f
+        ),
+        color = color
+    )
+}
+
+@Composable
+fun LargeButtonText(
+    text: String,
+    color: Color = ChiplessColors.textPrimary
+) {
+    Text(
+        text = text,
+        style = ChiplessShadowStyle(
+            style = ChiplessTypography.sh1,
+            offsetX = -8f,
+            offsetY = 8f,
+            blurRadius = 20f
+        ),
+        color = color
+    )
+}
+
+@Composable
+fun ActionButtonText(
+    text: String,
+    color: Color = ChiplessColors.textPrimary
+) {
+    Text(
+        text = text,
+        style = ChiplessShadowStyle(
+            style = ChiplessTypography.sh2,
+            offsetX = -8f,
+            offsetY = 8f,
+            blurRadius = 20f
+        ),
+        color = color
+    )
+}
