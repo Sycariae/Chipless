9c260e048edc00f16e872604f439838107aa7151
Sycariae <superhydro8@gmail.com>
2025-02-06 15:04:52 +0000

    Added PlayerLabels to GameTableScreen for testing glowIntensity implementation

--- app/src/main/java/com/sycarias/chipless/ui/screens/GameTable.kt
+++ app/src/main/java/com/sycarias/chipless/ui/screens/GameTable.kt
@@ -9,6 +9,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.sycarias.chipless.ui.composables.GlowIntensity
+import com.sycarias.chipless.ui.composables.PlayerLabel
 import com.sycarias.chipless.ui.composables.presets.ActionButton
 import com.sycarias.chipless.ui.composables.presets.ActionButtonText
 import com.sycarias.chipless.ui.composables.presets.Heading
@@ -72,5 +74,44 @@ fun GameTableScreen(navController: NavController, viewModel: TableDataViewModel)
         ActionButton(
             onClick = { players.incrementFocusPlayer() }
         ) { ActionButtonText(text = "Next Player") }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        val playerNames by remember {
+            derivedStateOf {
+                listOf(
+                    players.focusPlayer.name,
+                    players.getPlayer(players.getNextInActiveIDs(players.focusID, increment = 1).value).name,
+                    players.getPlayer(players.getNextInActiveIDs(players.focusID, increment = 2).value).name,
+                    players.getPlayer(players.getNextInActiveIDs(players.focusID, increment = 3).value).name
+                )
+            }
+        }
+
+        PlayerLabel(
+            name = playerNames[0],
+            glowIntensity = if (playerNames[0] == players.dealerPlayer.name) { GlowIntensity.HIGH } else { GlowIntensity.LOW }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        PlayerLabel(
+            name = playerNames[1],
+            glowIntensity = if (playerNames[1] == players.dealerPlayer.name) { GlowIntensity.HIGH } else { GlowIntensity.LOW }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        PlayerLabel(
+            name = playerNames[2],
+            glowIntensity = if (playerNames[2] == players.dealerPlayer.name) { GlowIntensity.HIGH } else { GlowIntensity.LOW }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        PlayerLabel(
+            name = playerNames[3],
+            glowIntensity = if (playerNames[3] == players.dealerPlayer.name) { GlowIntensity.HIGH } else { GlowIntensity.LOW }
+        )
     }
 }
