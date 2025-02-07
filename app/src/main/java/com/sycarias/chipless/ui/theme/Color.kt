ef7224723560e5649bd7e43db6b671ddf44633a4
Sycariae <superhydro8@gmail.com>
2025-02-07 10:24:59 +0000

    Removed redundant error color; Added greyOut color for greyed out elements; Adjusted comments to describe main function

--- app/src/main/java/com/sycarias/chipless/ui/theme/Color.kt
+++ app/src/main/java/com/sycarias/chipless/ui/theme/Color.kt
@@ -4,22 +4,22 @@ import androidx.compose.ui.graphics.Color
 
 // Define your custom colours here
 val ChiplessColors = ChiplessColorScheme(
-    primary = Color(0xFFFF4F77),        // Hot Pink
-    secondary = Color(0xFF37373C),      // Grey
+    primary = Color(0xFFFF4F77),        // Hot Pink - Focus Elements and Glows
+    secondary = Color(0xFF37373C),      // Grey - Main Elements
 
-    accentPrimary = Color(0xFFFF7292),  // Hot Pink
-    accentSecondary = Color(0xFF55555A),// Grey
+    accentPrimary = Color(0xFFFF7292),  // Hot Pink - Errors
+    accentSecondary = Color(0xFF55555A),// Grey - Element Decorators
 
-    textPrimary = Color(0xFFFFFFFF),    // White
-    textSecondary = Color(0xFFE2E2E2),  // Light Grey
-    textTertiary = Color(0xFF969699),   // Light-ish Grey
+    textPrimary = Color(0xFFFFFFFF),    // White - Main Text
+    textSecondary = Color(0xFFE2E2E2),  // Light Grey - Secondary Text
+    textTertiary = Color(0xFF969699),   // Light-ish Grey - Greyed Out Text
 
-    bgPrimary = Color(0xFF222225),      // Dark Grey
+    bgPrimary = Color(0xFF222225),      // Dark Grey - Main Background Color
     bgSecondary = Color(0xFF2C2C30),    // Lighter Dark Grey
 
-    success = Color(0xFF35FF92),        // Jade Green
-    warning = Color(0xFFFF9255),        // Orange
-    error = Color(0xFFEB3333)           // Red
+    success = Color(0xFF35FF92),        // Jade Green - Confirmation
+    warning = Color(0xFFFF9255),        // Orange - Warnings
+    greyOut = Color(0xFF2A2A2E)         // Dark-ish Grey - Greyed Out Elements
 )
 
 // Define your own custom color scheme class
@@ -39,5 +39,5 @@ data class ChiplessColorScheme(
 
     val success: Color,
     val warning: Color,
-    val error: Color
+    val greyOut: Color
 )
