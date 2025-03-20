7a9a8d84ac2b088db3c1a8c59d521acf14a66391
Sycariae <superhydro8@gmail.com>
2025-03-20 13:32:59 +0000

    Adjusted TableConfig() to support setting and getting without the use of `.value` in TableConfig.kt

--- app/src/main/java/com/sycarias/chipless/viewModel/TableConfig.kt
+++ app/src/main/java/com/sycarias/chipless/viewModel/TableConfig.kt
@@ -1,12 +1,14 @@
 package com.sycarias.chipless.viewModel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
 
 class TableConfig {
     // The starting balance given out at the start of a match
-    val startingChips = mutableIntStateOf(1000)
+    var startingChips by mutableIntStateOf(1000)
     // The minimum bet and the forced bet made by 2nd the player after the dealer at the start of a match
-    val bigBlind = mutableIntStateOf(10)
+    var bigBlind by mutableIntStateOf(10)
     // The forced bet made by the player after the dealer at the start of a match
-    val smallBlind = mutableIntStateOf(5)
+    var smallBlind by mutableIntStateOf(5)
 }
